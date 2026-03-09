b4460d26db7d58f215cc9b3f3ff63372cbe309b9
LyuLucas1207 <2993798118@qq.com>
2026-03-08 20:45:42 -0700

    Add i18n export to languages index file

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -3,6 +3,7 @@
  */
 export * from "./hooks";
 export * from "./languages";
+export { default as i18n } from "./languages/i18n";
 export { default as LanguageProvider } from "./providers";
 export * from "./providers";
 export * from "./resources";
